4b5b82791a1e9f2952f08c1d305a69b7dd9265e4
Emily <yinghua0111@gmail.com>
2015-02-11 15:56:53 +0800

    做了Login一部分  還沒有主板頁面

---
+++ SmartShoppingWebsite/Controllers/LogInController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SmartShoppingWebsite.Models;
+using System.Web.Security;
+
+namespace SmartShoppingWebsite.Controllers
+{
+    public class LogInController : Controller
+    {
+        // GET: LogIn
+        SmartShoppingEntities db = new SmartShoppingEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Members member)
+        {
+            //先做一個salt再存Password
+            System.Security.Cryptography.RNGCryptoServiceProvider rng = new System.Security.Cryptography.RNGCryptoServiceProvider();
+            byte[] buf = new byte[15];
+            rng.GetBytes(buf);
+            string salt = Convert.ToBase64String(buf);
+            member.Salt = salt;
+            string Password = member.Password;
+            Password = FormsAuthentication.HashPasswordForStoringInConfigFile(Password + salt, "sha1");
+            member.Password = Password;
+            
+            db.Members.Add(member);
+            db.SaveChanges();
+            return View();
+        }
+
+    }
+}
